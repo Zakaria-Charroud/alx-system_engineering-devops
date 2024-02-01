684340de05efd933779cc5167cede3d9b93ed894
Zakaria-Charroud <126858108+Zakaria-Charroud@users.noreply.github.com>
2024-02-01 15:11:01 +0100

    updatee

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/h.n/).join
+input = ARGV[0]
+
+# Check if the input contains the pattern /h.n/ and join the matches.
+matches = input.scan(/h.n/).join
+
+# Output the matches.
+puts matches
